ff64a1e681fa9e6b5f708d131ea96573f61dbb30
Adolfo Garcia Veytia (puerco) <puerco@chainguard.dev>
2023-09-09 19:43:35 -0600

    Move RerefenceToPurl to convert package
    Signed-off-by: Adolfo Garcia Veytia (puerco) <puerco@chainguard.dev>

--- pkg/convert/convert.go
+++ pkg/convert/convert.go
@@ -1,7 +1,12 @@
 package convert
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/openvex/go-vex/pkg/vex"
+	purl "github.com/package-url/packageurl-go"
 	v2 "github.com/wolfi-dev/wolfictl/pkg/configs/advisory/v2"
 )
 
@@ -31,4 +36,69 @@ func EventToVEXStatus(evt v2.Event) vex.Status {
 	default:
 		return ""
 	}
+}
+
+// PurlFromRef returns a purl capturing the image reference
+func PurlFromRef(imageRef string) (string, error) {
+	ref, err := name.ParseReference(imageRef)
+	if err != nil {
+		return "", fmt.Errorf("parsing image reference: %w", err)
+	}
+
+	digestVersion := ""
+
+	if digest, ok := ref.(name.Digest); ok {
+		digestVersion = digest.DigestStr()
+	}
+
+	qMap := map[string]string{}
+
+	tag, ok := ref.(name.Tag)
+	tagString := ""
+	if ok {
+		tagString = tag.TagStr()
+		// If the tag is latest, then make sure it is in the original
+		// reference string. Do not add it to the purl if it was inferred
+		if tagString == "latest" {
+			if !strings.HasSuffix(imageRef, ":latest") && !strings.Contains(imageRef, ":latest@") {
+				tagString = ""
+			}
+		}
+	}
+
+	if tagString != "" {
+		qMap["tag"] = tagString
+	}
+
+	parts := strings.Split(ref.Context().Name(), "/")
+	name := parts[len(parts)-1]
+
+	repoURL := ""
+	if len(parts) > 1 {
+		repoURL = strings.Join(parts[0:len(parts)-1], "/")
+		if parts[0] == "index.docker.io" {
+			if !strings.Contains(imageRef, "index.docker.io/library") {
+				repoURL = ""
+			}
+		}
+	}
+
+	if repoURL != "" {
+		qMap["repository_url"] = repoURL
+	}
+
+	p := purl.NewPackageURL(
+		purl.TypeOCI,  // Always set to "oci"
+		"",            // OCI purls don't have a namespace
+		name,          // Name of the image, no slashes
+		digestVersion, // We only add the digest as version if it was set in the original string
+		purl.QualifiersFromMap(qMap),
+		"",
+	)
+
+	purlString := p.ToString()
+
+	// This is a bug in the purl go implementation:
+	purlString = strings.ReplaceAll(purlString, "@sha256:", "@sha256%3A")
+	return purlString, nil
 }
